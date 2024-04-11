55123fb1505acf3cabca7b34d8f9506610f1f197
Antovigo <antvig@protonmail.com>
2024-04-10 22:55:03 -0400

    NIG model

--- preamble.R
+++ preamble.R
@@ -23,15 +23,25 @@ replace_na = function(data, x, default){
 	mutate(data, !!x := ifelse(is.na(!!x), default, !!x))
 }
 
+# Extract center of bins from cut()
+bin_center = function(x){
+    return(
+        sapply(x, function(name){mean(as.numeric(unlist(strsplit(gsub("\\(|\\]", "", name), ","))))})
+        )
+    }
+
 # Axes
 pretty_log_breaks = function(x) unique(10^(seq(floor(min(log10(x))) - 1, ceiling(max(log10(x))) + 1, 1)))
 
 xlog = function(lower = NULL, upper = NULL, ...){ return(scale_x_continuous(limits = c(lower, upper), trans = log10_trans(), labels = trans_format("log10", math_format(10^.x)), expand = c(0,0), ...))}
 ylog = function(lower = NULL, upper = NULL, ...){ return(scale_y_continuous(limits = c(lower, upper), trans = log10_trans(), labels = trans_format("log10", math_format(10^.x)), expand = c(0,0), ...))}
-logticks = function(s = 'bl', ...) annotation_logticks(size = 0.2, sides = s, ...)
+logticks = function(s = 'bl', lw = 0.2, ...) annotation_logticks(sides = s, linewidth = lw, ...)
 
 xcont = function(lower = NULL, upper = NULL, ...){ return(scale_x_continuous(limits = c(lower, upper), expand = c(0, 0), ...))}
 ycont = function(lower = NULL, upper = NULL, ...){ return(scale_y_continuous(limits = c(lower, upper), expand = c(0, 0), ...))}
+
+# Titled axis text
+tilt_text = function(angle = -45, hjust = 0.1, vjust = 0.1, size = 8) theme(axis.text.x = element_text(angle = angle, hjust = hjust, vjust = vjust, size = size)) 
 
 # Plot size
 size=function(h,w){
@@ -49,8 +59,32 @@ tall = function(){size(5, plot_w)}
 short = function(){size(2.5, plot_w)}
 square = function(){size(2.5, 2.5)}
 
-psave=function(name, device = png){
-	ggsave(name, width=plot_w, height=plot_h, dpi = 300, device = device)
+library(tools)
+
+psave <- function(name) {
+  # Extract folder name from the path
+  folder <- dirname(name)
+  
+  # Check if "PNG" folder exists, if not create it
+  pdf_folder <- file.path(folder, "PDF")
+  if (!file.exists(pdf_folder)) {
+    dir.create(pdf_folder)
+  }
+    
+  # Extract plot name
+  plot_name <- file_path_sans_ext(basename(name))
+    
+  # Save plot as PNG
+  png_path <- file.path(folder, paste0(plot_name, ".png"))
+  ggsave(png_path, width=plot_w, height=plot_h, dpi = 300, device = "png")
+  
+  # Save plot as PNG in the "PNG" folder
+  pdf_path <- file.path(pdf_folder, paste0(plot_name, ".pdf"))
+  ggsave(pdf_path, width=plot_w, height=plot_h, dpi = 300, device = cairo_pdf)
+}
+
+psave_only=function(name){
+	ggsave(name, width=plot_w, height=plot_h, dpi = 300)
 }
 
 ######################
@@ -59,22 +93,16 @@ psave=function(name, device = png){
 
 library(HDInterval) # highest density credible intervals
 
-# Closed-form Beta-Binomial credible interval
-default_prior1 = 0.5
-default_prior2 = 0.5
-binom_mean = function(k, n, conf, prior1 = default_prior1, prior2 = default_prior2, ...) binom.bayes(k, n, prior.shape1 = prior1, prior.shape2 = prior2, conf.level = conf, ...)$mean
-binom_high = function(k, n, conf, prior1 = default_prior1, prior2 = default_prior2, ...) binom.bayes(k, n, prior.shape1 = prior1, prior.shape2 = prior2, conf.level = conf, ...)$upper
-binom_low = function(k, n, conf, prior1 = default_prior1, prior2 = default_prior2, ...) binom.bayes(k, n, prior.shape1 = prior1, prior.shape2 = prior2, conf.level = conf, ...)$lower
-
-# Closed-form Beta-binomial posterior parameter estimates. k and n may/may not be vectors.
+# Closed-form Beta-binomial posterior parameter estimates. k and n may/may not be vectors. (1,1): uniform prior. (0.5,0.5): Jeffreys prior.
 binom_def_prior_a = 0.5
 binom_def_prior_b = 0.5
+def_conf = 0.99
 
 binom_post_a = function(k, n, prior_a = binom_def_prior_a, prior_b = binom_def_prior_b, ...) prior_a + sum(k)
 binom_post_b = function(k, n, prior_a = binom_def_prior_a, prior_b = binom_def_prior_b, ...) prior_b + sum(n - k)
 binom_post_mean = function(k, n, ...) binom_post_a(k, n, ...) / (binom_post_a(k, n, ...) + binom_post_b(k, n, ...))
-binom_hdi_low = function(k, n, conf = 0.95, ...) hdi(qbeta, conf, shape1 = binom_post_a(k, n, ...), shape2 = binom_post_b(k, n, ...))[1]
-binom_hdi_high = function(k, n, conf = 0.95, ...) hdi(qbeta, conf, shape1 = binom_post_a(k, n, ...), shape2 = binom_post_b(k, n, ...))[2]
+binom_hdi_low = function(k, n, conf = def_conf, ...) hdi(qbeta, conf, shape1 = binom_post_a(k, n, ...), shape2 = binom_post_b(k, n, ...))[1]
+binom_hdi_high = function(k, n, conf = def_conf, ...) hdi(qbeta, conf, shape1 = binom_post_a(k, n, ...), shape2 = binom_post_b(k, n, ...))[2]
 
 # Closed-form Gamma-Poisson
 poisson_def_prior_a = 0.0
@@ -86,9 +114,39 @@ poisson_post_mean = function(k, prior_a = poisson_def_prior_a, prior_b = poisson
 poisson_hdi_low = function(k, conf = 0.95, ...) hdi(qgamma, conf, shape = poisson_post_a(k, ...), rate = poisson_post_b(k, ...))[1]
 poisson_hdi_high = function(k, conf = 0.95, ...) hdi(qgamma, conf, shape = poisson_post_a(k, ...), rate = poisson_post_b(k, ...))[2]
 
-# Closed-form Normal
-normal_def_prior_mu = 0.0
-normal_def_prior_sigma = 1.0
+# Normal-inverse-gamma updating. Mean ~ N(mu, sigma), Var ~ IG(alpha, beta)
+norm_post_NIG = function(x, prior_mu, prior_sigma, conf = def_conf, prior_alpha = 1){
+
+    # Calculate the posterior parameters
+    n <- length(x)
+    x_bar <- mean(x)
+    s_sq <- var(x)
+    
+    # Uninformative prior for beta: sample variance
+    prior_beta = (n-1) * s_sq / 2
+    prior_lambda <- 1 / (prior_sigma^2) # precision
+
+    post_mu <- (prior_lambda * prior_mu + n * x_bar) / (prior_lambda + n)
+    post_lambda <- prior_lambda + n
+    post_alpha <- prior_alpha + n/2
+    post_beta <- prior_beta + (n * s_sq + (prior_lambda * n * (x_bar - prior_mu)^2) / (prior_lambda + n)) / 2
+    
+    post_sigma = sqrt(1 / post_lambda)
+    
+    # Calculate the parameters of the marginal distribution of mu (integrating away sigma)
+    scale <- sqrt(post_beta / (post_lambda * post_alpha))
+
+    # Get the credible interval
+    conf_tail = (1 - conf) / 2
+    post_mu_low <- qt(conf_tail, df = 2 * post_alpha) * scale + post_mu
+    post_mu_high <- qt(1 - conf_tail, df = 2 * post_alpha) * scale + post_mu
+
+    return(c(post_mu, post_mu_low, post_mu_high))
+}
+
+norm_post_mu = function(x, prior_mu, prior_sigma, prior_alpha = 1) norm_post_NIG(x, prior_mu, prior_sigma, prior_alpha = prior_alpha)[1]
+norm_post_low = function(x, prior_mu, prior_sigma, prior_alpha = 1) norm_post_NIG(x, prior_mu, prior_sigma, prior_alpha = prior_alpha)[2]
+norm_post_high = function(x, prior_mu, prior_sigma, prior_alpha = 1) norm_post_NIG(x, prior_mu, prior_sigma, prior_alpha = prior_alpha)[3]
 
 ##############
 ### Ricing ###
@@ -155,11 +213,11 @@ scale_fill_adaptive <- function(palette, alpha = 1, use_raw = FALSE, ...) {
 }
 
 # Backup palette (e.g. for datetime)
-scale_color_wes = function(...){
+scale_color_def = function(...){
     scale_color_gradientn(colors = wes_palette('Zissou1'), ...)
 }
 
-scale_fill_wes = function(...){
+scale_fill_def = function(...){
     scale_fill_gradientn(colors = wes_palette('Zissou1'), ...)
 }
 
@@ -172,6 +230,7 @@ def_fill_c = function(){return (scale_fill_gradientn(colors = wes_palette("Zisso
 
 # Manual access
 Zissou1 = c("#3B9AB2", "#78B7C5", "#EBCC2A", "#E1AF00", "#F21A00")
+pal = c("#3B9AB2", "#78B7C5", "#EBCC2A", "#E1AF00", "#F21A00")
 
 options(
   ggplot2.discrete.colour = def_col_d, ggplot2.continuous.colour = def_col_c,
